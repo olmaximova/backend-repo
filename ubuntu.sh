9ec37cddfb695c8fc1022e48cb92bcce9ca07272
olmaximova <ol.maximova1309@gmail.com>
2026-05-17 16:30:03 +0300

    обновляет ubuntu.sh файл

--- ubuntu.sh
+++ ubuntu.sh
@@ -22,6 +22,7 @@ Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 sudo usermod -aG docker $USER
